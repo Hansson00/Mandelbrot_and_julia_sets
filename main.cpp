936d8178c79f3745d5b948055126d65f3e2d81ff
Isak Hansson <isakha@student.chalmers.se>
2022-09-17 13:07:29 +0200

    Added the external liberaries and modified the linker.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include "iostream"
 #include "Engine.h"
-#include <cstdlib>
 #include "string"
-
-
-#define WIN_WIDTH 1920
-#define WIN_HEIGHT 1080
-
 
 Engine* engine = nullptr;
 
 int main(int argc, char* argv[]) {
 
-	engine = new Engine(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+	if (argc == 4) {
+		engine = new Engine(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
 
-	while (engine->running) {
-		engine->events();
+		while (engine->running) {
+			engine->events();
+		}
+		delete(engine);
 	}
-	delete(engine);
+	else
+		std::cout << "Error, invalid arguments...\n";
 	return 0;
 }
